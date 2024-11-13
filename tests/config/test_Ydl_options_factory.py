dcef68173653584c597a26e73d88c0c6e98593c1
Matthieu D. <orkeilius@gmail.com>
2024-11-13 15:01:49 +0000

    extract ydl_option from tubeUp

---
+++ tests/config/test_Ydl_options_factory.py
@@ -0,0 +1,182 @@
+import logging
+from unittest import TestCase
+import os
+import unittest
+from tubeup.Helper.DirPath import DirPath
+from tubeup.TubeUp import TubeUp
+from tubeup.config.Ydl_options_factory import *
+
+
+dir_path = DirPath("~/.tubeup'")
+
+def mocked_ydl_progress_hook(d):
+    pass
+
+class Ydl_options_factoryTest(unittest.TestCase):
+    
+    logger = logging.getLogger(__name__)
+    
+    def test_generate_ydl_options_with_download_archive(self):
+            result = Ydl_options_factory.generate_ydl_options(dir_path,self.logger,
+                                                mocked_ydl_progress_hook,
+                                                use_download_archive=True)
+
+            expected_result = {
+                'outtmpl': os.path.join(
+                    dir_path.downloads, '%(id)s.%(ext)s'),
+                'restrictfilenames': True,
+                'verbose': False,
+                'quiet': True,
+                'download_archive': os.path.join(dir_path.root,
+                                                 '.ytdlarchive'),
+                'progress_with_newline': True,
+                'forcetitle': True,
+                'continuedl': True,
+                'retries': 9001,
+                'fragment_retries': 9001,
+                'forcejson': False,
+                'writeinfojson': True,
+                'writedescription': True,
+                'writethumbnail': True,
+                'writeannotations': True,
+                'writesubtitles': True,
+                'allsubtitles': True,
+                'ignoreerrors': True,
+                'fixup': 'warn',
+                'nooverwrites': True,
+                'consoletitle': True,
+                'prefer_ffmpeg': True,
+                'call_home': False,
+                'logger': self.logger,
+                'progress_hooks': [mocked_ydl_progress_hook]}
+            print(expected_result)
+            self.assertEqual(result, expected_result)
+
+    def test_generate_ydl_options(self):
+        result = Ydl_options_factory.generate_ydl_options(dir_path,self.logger,mocked_ydl_progress_hook)
+        expected_result = {
+            'outtmpl': os.path.join(
+                dir_path.downloads, '%(id)s.%(ext)s'),
+            'restrictfilenames': True,
+            'verbose': False,
+            'quiet': True,
+            'progress_with_newline': True,
+            'forcetitle': True,
+            'continuedl': True,
+            'retries': 9001,
+            'fragment_retries': 9001,
+            'forcejson': False,
+            'writeinfojson': True,
+            'writedescription': True,
+            'writethumbnail': True,
+            'writeannotations': True,
+            'writesubtitles': True,
+            'allsubtitles': True,
+            'ignoreerrors': True,
+            'fixup': 'warn',
+            'nooverwrites': True,
+            'consoletitle': True,
+            'prefer_ffmpeg': True,
+            'call_home': False,
+            'logger': self.logger,
+            'progress_hooks': [mocked_ydl_progress_hook]}
+        self.assertEqual(result, expected_result)
+
+    def test_generate_ydl_options_with_proxy(self):
+        result = Ydl_options_factory.generate_ydl_options(dir_path,self.logger,
+            mocked_ydl_progress_hook, proxy_url='http://proxytest.com:8080')
+        expected_result = {
+            'outtmpl': os.path.join(
+                dir_path.downloads, '%(id)s.%(ext)s'),
+            'restrictfilenames': True,
+            'verbose': False,
+            'quiet': True,
+            'progress_with_newline': True,
+            'forcetitle': True,
+            'continuedl': True,
+            'retries': 9001,
+            'fragment_retries': 9001,
+            'forcejson': False,
+            'writeinfojson': True,
+            'writedescription': True,
+            'writethumbnail': True,
+            'writeannotations': True,
+            'writesubtitles': True,
+            'allsubtitles': True,
+            'ignoreerrors': True,
+            'fixup': 'warn',
+            'nooverwrites': True,
+            'consoletitle': True,
+            'prefer_ffmpeg': True,
+            'call_home': False,
+            'logger': self.logger,
+            'progress_hooks': [mocked_ydl_progress_hook],
+            'proxy': 'http://proxytest.com:8080'}
+        self.assertEqual(result, expected_result)
+    
+    def test_generate_ydl_options_with_ydl_account(self):
+        result = Ydl_options_factory.generate_ydl_options(dir_path,self.logger,
+            mocked_ydl_progress_hook, ydl_username='testUsername',
+            ydl_password='testPassword')
+        expected_result = {
+            'outtmpl': os.path.join(
+                dir_path.downloads, '%(id)s.%(ext)s'),
+            'restrictfilenames': True,
+            'verbose': False,
+            'quiet': True,
+            'progress_with_newline': True,
+            'forcetitle': True,
+            'continuedl': True,
+            'retries': 9001,
+            'fragment_retries': 9001,
+            'forcejson': False,
+            'writeinfojson': True,
+            'writedescription': True,
+            'writethumbnail': True,
+            'writeannotations': True,
+            'writesubtitles': True,
+            'allsubtitles': True,
+            'ignoreerrors': True,
+            'fixup': 'warn',
+            'nooverwrites': True,
+            'consoletitle': True,
+            'prefer_ffmpeg': True,
+            'call_home': False,
+            'logger': self.logger,
+            'progress_hooks': [mocked_ydl_progress_hook],
+            'username': 'testUsername',
+            'password': 'testPassword'}
+        self.assertEqual(result, expected_result)
+    def test_generate_ydl_options_with_verbose_mode(self):
+        result = Ydl_options_factory.generate_ydl_options(dir_path,self.logger,
+            mocked_ydl_progress_hook, ydl_username='testUsername',
+            ydl_password='testPassword',verbose=True)
+        expected_result = {
+            'outtmpl': os.path.join(
+                dir_path.downloads, '%(id)s.%(ext)s'),
+            'restrictfilenames': True,
+            'verbose': True,
+            'quiet': False,
+            'progress_with_newline': True,
+            'forcetitle': True,
+            'continuedl': True,
+            'retries': 9001,
+            'fragment_retries': 9001,
+            'forcejson': False,
+            'writeinfojson': True,
+            'writedescription': True,
+            'writethumbnail': True,
+            'writeannotations': True,
+            'writesubtitles': True,
+            'allsubtitles': True,
+            'ignoreerrors': True,
+            'fixup': 'warn',
+            'nooverwrites': True,
+            'consoletitle': True,
+            'prefer_ffmpeg': True,
+            'call_home': False,
+            'logger': self.logger,
+            'progress_hooks': [mocked_ydl_progress_hook],
+            'username': 'testUsername',
+            'password': 'testPassword'}
+        self.assertEqual(result, expected_result)
--- tubeup/TubeUp.py
+++ tubeup/TubeUp.py
@@ -18,10 +18,7 @@ from logging import getLogger
 from urllib.parse import urlparse
 
 from tubeup import __version__
-
-
-DOWNLOAD_DIR_NAME = 'downloads'
-
+from tubeup.config.Ydl_options_factory import *
 
 class TubeUp(object):
 
@@ -147,7 +144,7 @@ class TubeUp(object):
                 if self.verbose:
                     print(msg)
 
-        ydl_opts = self.generate_ydl_options(ydl_progress_hook,
+        ydl_opts = Ydl_options_factory.generate_ydl_options(self.dir_path,self.logger,ydl_progress_hook,
                                              cookie_file, proxy_url,
                                              ydl_username, ydl_password,
                                              use_download_archive)
@@ -205,87 +202,6 @@ class TubeUp(object):
 
         return basenames
 
-    def generate_ydl_options(self,
-                             ydl_progress_hook,
-                             cookie_file=None,
-                             proxy_url=None,
-                             ydl_username=None,
-                             ydl_password=None,
-                             use_download_archive=False,
-                             ydl_output_template=None):
-        """
-        Generate a dictionary that contains options that will be used
-        by yt-dlp.
-
-        :param ydl_progress_hook:     A function that will be called during the
-                                      download process by youtube_dl.
-        :param proxy_url:             A proxy url for YoutubeDL.
-        :param ydl_username:          Username that will be used to download the
-                                      resources with youtube_dl.
-        :param ydl_password:          Password of the related username, will be
-                                      used to download the resources with
-                                      youtube_dl.
-        :param use_download_archive:  Record the video url to the download archive.
-                                      This will download only videos not listed in
-                                      the archive file. Record the IDs of all
-                                      downloaded videos in it.
-        :return:                      A dictionary that contains options that will
-                                      be used by youtube_dl.
-        """
-        ydl_opts = {
-            'outtmpl': os.path.join(self.dir_path.downloads,
-                                    self.output_template),
-            'restrictfilenames': True,
-            'quiet': not self.verbose,
-            'verbose': self.verbose,
-            'progress_with_newline': True,
-            'forcetitle': True,
-            'continuedl': True,
-            'retries': 9001,
-            'fragment_retries': 9001,
-            'forcejson': False,
-            'writeinfojson': True,
-            'writedescription': True,
-            'writethumbnail': True,
-            'writeannotations': True,
-            'writesubtitles': True,
-            'allsubtitles': True,
-            'ignoreerrors': True,  # Geo-blocked,
-                                   # copyrighted/private/deleted
-                                   # will be printed to STDOUT and channel
-                                   # ripping will  continue uninterupted,
-                                   # use with verbose off
-            'fixup': 'warn',  # Slightly more verbosity for debugging
-                              # problems
-            'nooverwrites': True,  # Don't touch what's already been
-                                   # downloaded speeds things
-            'consoletitle': True,   # Download percentage in console title
-            'prefer_ffmpeg': True,  # `ffmpeg` is better than `avconv`,
-                                    # let's prefer it's use
-            # Warns on out of date youtube-dl script, helps debugging for
-            # youtube-dl devs
-            'call_home': False,
-            'logger': self.logger,
-            'progress_hooks': [ydl_progress_hook]
-        }
-
-        if cookie_file is not None:
-            ydl_opts['cookiefile'] = cookie_file
-
-        if proxy_url is not None:
-            ydl_opts['proxy'] = proxy_url
-
-        if ydl_username is not None:
-            ydl_opts['username'] = ydl_username
-
-        if ydl_password is not None:
-            ydl_opts['password'] = ydl_password
-
-        if use_download_archive:
-            ydl_opts['download_archive'] = os.path.join(self.dir_path.root,
-                                                        '.ytdlarchive')
-
-        return ydl_opts
 
     def upload_ia(self, videobasename, custom_meta=None):
         """
